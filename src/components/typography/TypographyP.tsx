91c598bc34525515195dde78760a29720f7e01d8
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2025-08-27 19:14:42 +0200

    Some starting typography based on shadcn.

---
+++ src/components/typography/TypographyP.tsx
@@ -0,0 +1,4 @@
+// base typography for p
+export function TypographyP({ children }: { children?: React.ReactNode }) {
+  return <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>;
+}
